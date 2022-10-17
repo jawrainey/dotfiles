f7983a9b7f1318c164e4725d59e237511e22ecc2
Jay Rainey <jawrainey@gmail.com>
2022-10-17 22:32:40 +0100

    Refactors deps

--- deps.sh
+++ deps.sh
@@ -1,27 +1,46 @@
-if [[ "$*" == *"fresh"* ]]; then
-  # Useful for install python versions
-  sudo apt-get install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
-  
+# These commands are mosty helper methods when moving between machines.
+
+if [[ "$*" == *"vim"* ]]; then
+  vundle="$HOME/.vim/bundle/Vundle.vim"
+
+  if [ -d "$vundle" ]; then
+    cd $vundle; git pull; cd -;
+  else
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  fi;
+fi
+
+if [[ "$*" == *"python"* ]]; then
   # pyenv: python version manager
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  cd $HOME/.pyenv && src/configure && make -C src && cd $HOME
+  cd $HOME/.pyenv && make -C src && cd $HOME
 
   # poetry: python package dependency manager
   curl -sSL https://install.python-poetry.org | python3 -
+fi
 
-  # fzf: auto complete for history/search
-  cd $HOME && \
-  git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && \
-  cd $HOME/.fzf && ./install --no-bash --no-fish --key-bindings --completion --update-rc
+if [[ "$*" == *"fzf"* ]]; then
+  fzf="$HOME/.fzf"
 
-  # TODO: what about zsh?
-  sudo apt install zsh
+  if [ -d "$fzf" ]; then
+    cd $fzf && git pull && cd -
+  else
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    cd $HOME/.fzf && ./install --no-bash --no-fish --key-bindings --completion --update-rc
+  fi;
+fi
 
-  # zsh plugins
-  git clone https://github.com/sunlei/zsh-ssh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-ssh
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [[ "$*" == *"zshp"* ]]; then
+  plugins="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins"
+  git clone https://github.com/sunlei/zsh-ssh $plugins/zsh-ssh
+  git clone https://github.com/zsh-users/zsh-autosuggestions $plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $plugins/zsh-syntax-highlighting
+fi
 
-  # TODO: what about fzf?
-
-  # sudo apt install bat
+if [[ "$*" == *"linux"* ]]; then
+  sudo apt-get install \
+    zsh \ 
+    bat \
+    # Useful for install python versions
+    build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
 fi
